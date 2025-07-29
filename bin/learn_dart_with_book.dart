b5ba2dbf08c83c61de9776c9e839816802447e37
JongHyun070105 <bobi09361@gmail.com>
2025-07-29 21:34:01 +0900

    Initial commit

---
+++ bin/learn_dart_with_book.dart
@@ -0,0 +1,5 @@
+import 'package:learn_dart_with_book/learn_dart_with_book.dart' as learn_dart_with_book;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learn_dart_with_book.calculate()}!');
+}
---
+++ test/learn_dart_with_book_test.dart
@@ -0,0 +1,8 @@
+import 'package:learn_dart_with_book/learn_dart_with_book.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
