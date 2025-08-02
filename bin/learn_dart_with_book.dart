900c3ce6d58ddb9d41d89e0af5819cd866465c79
JongHyun070105 <bobi09361@gmail.com>
2025-08-02 21:39:13 +0900

    -

--- bin/learn_dart_with_book.dart
+++
@@ -1,5 +0,0 @@
-import 'package:learn_dart_with_book/learn_dart_with_book.dart' as learn_dart_with_book;
-
-void main(List<String> arguments) {
-  print('Hello world: ${learn_dart_with_book.calculate()}!');
-}
--- test/learn_dart_with_book_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:learn_dart_with_book/learn_dart_with_book.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
