f2960e8c885aca4a90bd9e507b1007ae97d5b0d2
JongHyun070105 <bobi09361@gmail.com>
2025-08-02 21:54:35 +0900

    Learn variable_type

---
+++ lib/chapter01/variable_type.dart
@@ -0,0 +1,18 @@
+void main() {
+  // String : 문자열
+  String name = '종현';
+
+  // int : 정수
+  int age = 19;
+
+  // double : 실수
+  double height = 180.5;
+
+  // bool : 불리언 (true & false)
+  bool isTrue = true;
+
+  print(name);
+  print(age);
+  print(height);
+  print(isTrue);
+}
